8fa6e65ba4912ac4bd6f518e266cf946db183463
Nahom-Derese <nahomderese@gmail.com>
2023-06-13 22:41:49 +0300

    Add EVENT API CALL DONE

--- lib/Controllers/add_event_controller.dart
+++ lib/Controllers/add_event_controller.dart
@@ -1,8 +1,11 @@
 import 'dart:async';
-import 'package:get/get.dart';
+import 'package:dio/dio.dart';
+import 'package:get/get.dart' hide FormData, MultipartFile;
 import 'package:flutter/material.dart';
+import 'package:on_reserve/Models/user.dart';
 import 'package:on_reserve/helpers/log/logger.dart';
 import 'package:on_reserve/helpers/network/network_provider.dart';
+import 'package:on_reserve/helpers/storage/secure_store.dart';
 
 class AddEventController extends GetxController {
   var events = [];
@@ -15,15 +18,44 @@ class AddEventController extends GetxController {
     // getEvents();
   }
 
-  Future<List> getHistory() async {
-    var response = await NetworkHandler.get(endpoint: 'events/');
+  Future<bool> editProfile() async {
+    late FormData formData;
+
+    formData = new FormData.fromMap({
+      'companyId': '1',
+      'title': 'Yo Concert',
+      'desc': 'Yearly Concert',
+      'eventStartTime': '2023-06-01T00:00:00.000Z',
+      'eventEndTime': '2023-06-07T00:00:00.000Z',
+      'eventDeadline': '2023-06-02T00:59:38.034Z',
+      'economySeats': '100',
+      'economyPrice': '300',
+      'vipSeats': '100',
+      'vipPrice': '500',
+      'city': 'Adama',
+      'street': 'Mebrat',
+      'venue': 'Eagle Hotel',
+      'latitude': '100000',
+      'longitude': '30000',
+      'categories': '3',
+      'images':
+          await MultipartFile.fromFile('path/to/image', filename: 'AI.png')
+    });
+
+    var response =
+        await NetworkHandler.post(body: formData, endpoint: 'event/add/');
+
     if (response[1] == 200) {
       try {
-        events = response[0];
+        Map data = (response[0]);
+
+        update();
+        return true;
       } catch (e) {
-        logger(AddEventController).e("Error: $e");
+        logger(AddEventController).e(e);
+        return false;
       }
-    } else {}
-    return events;
+    }
+    return false;
   }
 }
